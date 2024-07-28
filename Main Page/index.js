c74c1eb8bb6e0af6a4fafd85eab36d09a3d63000
Abhishek Ghosh <abhishekp492002@gmail.com>
2024-07-28 20:48:47 +0530

    commit

--- Main Page/index.js
+++ Main Page/index.js
@@ -1,1 +1,16 @@
-document.getElementsByClassName
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req,res)=>{
+    res.sendFile(__dirname +"/index.html" )
+})
+
+
+app.listen(port, function(){
+    console.log("Server is running successfully at " + port);
+})
