21fdc073088fde81923d19e21dfe191cbe46c5d4
Abhishek Ghosh <abhishekp492002@gmail.com>
2024-08-25 13:55:45 +0530

    commit

--- Main Page/index.js
+++ Main Page/index.js
@@ -1,16 +1,18 @@
 import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
-app.use(express.static(__dirname))
+app.use(bodyParser.urlencoded({extended:true}))
 
+app.use(express.static("public"))
 
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname +"/index.html" )
+    res.render("index.ejs" )
 })
 
 
